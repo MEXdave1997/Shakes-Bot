2852d631fd5209b7692d8e0f80a8eb05662c1145
David Madrigal-Hernandez <david.mhernandez97@outlook.com>
2018-11-19 17:47:04 -0800

    initial commit with basic bot code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shakes Bot
+
+A simple Discord bot using [Discord.JS](https://github.com/discordjs/discord.js) framework for the **Seize the Day** Server.
---
+++ bot.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js")
+const Client = new Discord.Client()
+const config = require("./config.json")
+
+// sets prefix
+const prefix = config.prefix 
+Client.on('ready', () => {
+    console.log("I am ready!")
+})
+
+/// Welcomes a new member
+Client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find(ch => ch.name === 'welcome')
+
+    if (!channel) return
+
+    channel.send(`Welcome to the server, ${member}`)
+})
+
+// Sends messages to a channel based on certain commands
+Client.on('message', message => {
+
+    // Exit and stop if prefix is not there
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    if (message.content.startsWith(prefix + 'ping')) {
+        message.channel.send('pong!')
+    }
+
+    if (message.content.startsWith(config.prefix + 'foo')) {
+        message.channel.send('bar')
+    }
+    
+   if (message.content.startsWith(config.prefix + 'roles')) {
+       let roles = member.
+       message
+   }
+
+})
+
+Client.login(config.token)
